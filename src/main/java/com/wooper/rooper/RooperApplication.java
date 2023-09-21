ac958b8354c5807ee82b05f1b0daafdbdb0fc366
Jinny Park <jinny@Jinnyui-MacBookPro.local>
2023-09-21 23:20:14 +0900

    first commit

---
+++ src/main/java/com/wooper/rooper/RooperApplication.java
@@ -0,0 +1,13 @@
+package com.wooper.rooper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RooperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RooperApplication.class, args);
+	}
+
+}
